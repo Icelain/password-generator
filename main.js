44f68d93f01be7f93eae18d2a94a2c4959b3951c
Icelain <xerneas965@gmail.com>
2020-10-17 18:57:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function index(){
-
+    document.getElementById("icon-loader").style.display="inline";
     const Http =new XMLHttpRequest();
     const url="https://passwd-api.herokuapp.com/password";
     Http.open("GET",url);
@@ -7,6 +7,7 @@ function index(){
 
     Http.onreadystatechange=(e)=>{
         var uxie=JSON.parse(Http.responseText)
+        document.getElementById("icon-loader").style.display="none";
         document.getElementById("isekai").innerHTML=uxie.passwd;
     }
 
